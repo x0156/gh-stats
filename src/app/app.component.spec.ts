3358b4bc611447c78b4fa3c01b25e85ea13283c0
Karuna <paradox@outlook.in>
2017-07-20 13:28:18 +0530

    updated app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,28 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
-import { ReleaseDownloadsComponent } from './stats/release-downloads/release-downloads.component';
-import { StatsService } from './services/stats.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { RouterTestingModule } from '@angular/router/testing';
 import { APP_BASE_HREF } from '@angular/common';
+
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   beforeEach(async(() => {
-
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        ReleaseDownloadsComponent
+        AppComponent
       ],
-      providers: [StatsService, { provide: APP_BASE_HREF, useValue: '/' }],
-      imports: [MaterialModule, HttpModule,
-        RouterTestingModule.withRoutes([]),
-        NgxChartsModule
-        , BrowserAnimationsModule]
+      providers: [ { provide: APP_BASE_HREF, useValue: '/' }],
+      imports: [RouterTestingModule.withRoutes([])]
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.debugElement.componentInstance;
   }));
 
 
-  it('should create the app', async(() => {
+  it('should create the app', () => {
     expect(component).toBeTruthy();
-  }));
+  });
+
 
 });
